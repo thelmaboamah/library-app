21aa0b113df0a333ed1825330a889a4086f015ea
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-03-12 14:08:38 -0700

    Generate sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,17 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.create(user_params)
+		redirect_to root_path 
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+	
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :password)
 	end
 end
